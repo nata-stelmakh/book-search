90ddaceb90d2310977c6b3ad3ec349b2ebc286a5
Author Name <stelmakh.natallia@gmail.com>
2020-09-04 20:39:14 -0400

    Folder structure

---
+++ client/build/precache-manifest.d2cfe883e4f319bb4273b2b6204c6f9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4fa383367dc1124cd57",
+    "url": "/static/css/main.470cf168.chunk.css"
+  },
+  {
+    "revision": "a4fa383367dc1124cd57",
+    "url": "/static/js/main.4c138b2e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f14e103a85ded4a2db8",
+    "url": "/static/js/2.9f08725b.chunk.js"
+  },
+  {
+    "revision": "dbac9032eb88db8fb1129b342a426455",
+    "url": "/index.html"
+  }
+];
