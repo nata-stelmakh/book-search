f8f5bc8a1e3f1003d7a8f195555fcb319f82b5fc
Author Name <stelmakh.natallia@gmail.com>
2020-09-12 21:50:22 -0400

    added mongopassword

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://username1:password1@cluster0.abhsh.mongodb.net/book?retryWrites=true&w=majority",
+    "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
   {
     useCreateIndex: true,
     useNewUrlParser: true,
